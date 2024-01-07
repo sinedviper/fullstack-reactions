1b7c71ed67f0bc450fe668c4df19bf8ac2c1199e
Denis Repyev <45439759+sinedviper@users.noreply.github.com>
2024-01-07 20:30:01 +0200

    end of 14 section

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,13 +1,21 @@
-using AutoMapper;
+using Application.Activities;
 using Domain;
 
 namespace Application.Core
 {
-    public class MappingProfiles : Profile
+    public class MappingProfiles : AutoMapper.Profile
     {
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(d => d.HostUsername, 
+                    o => o.MapFrom(s => s.Attendees
+                        .FirstOrDefault(x=>x.IsHost).AppUser.UserName));
+            CreateMap<ActivityAttendee, Profile.Profile>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
         }
     }
 }
---
+++ Application/Profile/Profile.cs
@@ -0,0 +1,9 @@
+namespace Application.Profile;
+
+public class Profile
+{
+    public string Username { get; set; }
+    public string DisplayName { get; set; }
+    public string Bio { get; set; }
+    public string Image { get; set; }
+}
